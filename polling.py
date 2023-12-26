ac90e537be683efdb1b7f6dd08f3f33a13336885
AlexWoIf <alexey.barannikov@gmail.com>
2023-12-26 16:06:11 +0300

    fix vars names and first delay after request error

--- polling.py
+++ polling.py
@@ -10,7 +10,8 @@ from settings import DvmnSettings, LogSettings, TgSettings
 POLLING_URL = 'https://dvmn.org/api/long_polling/'
 
 
-@retry((ReadTimeout, ConnectionError), delay=1, max_delay=3600, backoff=2)
+@retry((ReadTimeout, ConnectionError),
+       delay=0, max_delay=3600, backoff=2, jitter=1, )
 def persistent_request(url, params, headers):
     logger.debug(f'Send request with {params=}')
     response = requests.get(url, params=params, headers=headers, )
@@ -45,10 +46,10 @@ if __name__ == '__main__':
     )
     logger = logging.getLogger(__name__)
     logger.info('Start logging')
-    tg = TgSettings()
-    dvmn = DvmnSettings()
-    bot = telegram.Bot(token=f'{tg.bot_token}')
-    for changes in check_status(dvmn.token):
+    tg_settings = TgSettings()
+    dvmn_settings = DvmnSettings()
+    bot = telegram.Bot(token=f'{tg_settings.bot_token}')
+    for changes in check_status(dvmn_settings.token):
         message = "Статус некоторых проверок изменился! " \
                   "Детали из ответа сервера:\n"
         for attempt in changes:
@@ -58,4 +59,4 @@ if __name__ == '__main__':
             message += f'Название урока: {attempt["lesson_title"]}\n' \
                 f'Ссылка на урок: {attempt["lesson_url"]}\n' \
                 f'Задание {negation}принято'
-        bot.send_message(text=message, chat_id=tg.chat_id)
+        bot.send_message(text=message, chat_id=tg_settings.chat_id)
