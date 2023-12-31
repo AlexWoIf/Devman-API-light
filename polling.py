53d1ef43f56fa2deb471ebda2b2fcd68c58f301b
Alexey Barannikov <alexey.barannikov@gmail.com>
2023-12-31 11:00:11 +0300

    catch errors and write to log

--- polling.py
+++ polling.py
@@ -10,6 +10,22 @@ from settings import DvmnSettings, LogSettings, TgSettings
 POLLING_URL = 'https://dvmn.org/api/long_polling/'
 
 
+logger = logging.getLogger(__name__)
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
 @retry((ReadTimeout, ConnectionError),
        delay=0, max_delay=3600, backoff=2, jitter=1, )
 def persistent_request(url, params, headers):
@@ -44,19 +60,23 @@ if __name__ == '__main__':
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=getattr(logging, log.level.upper(), None),
     )
-    logger = logging.getLogger(__name__)
-    logger.info('Start logging')
     tg_settings = TgSettings()
     dvmn_settings = DvmnSettings()
     bot = telegram.Bot(token=f'{tg_settings.bot_token}')
-    for changes in check_status(dvmn_settings.token):
-        message = "Статус некоторых проверок изменился! " \
-                  "Детали из ответа сервера:\n"
-        for attempt in changes:
-            negation = ''
-            if attempt["is_negative"]:
-                negation = 'не '
-            message += f'Название урока: {attempt["lesson_title"]}\n' \
-                f'Ссылка на урок: {attempt["lesson_url"]}\n' \
-                f'Задание {negation}принято'
-        bot.send_message(text=message, chat_id=tg_settings.chat_id)
+    logger.addHandler(TelegramLogsHandler(bot, tg_settings.chat_id))
+    logger.info('Start logging')
+    while True:
+        try:
+            for changes in check_status(dvmn_settings.token):
+                message = "Статус некоторых проверок изменился! " \
+                        "Детали из ответа сервера:\n"
+                for attempt in changes:
+                    negation = ''
+                    if attempt["is_negative"]:
+                        negation = 'не '
+                    message += f'Название урока: {attempt["lesson_title"]}\n' \
+                        f'Ссылка на урок: {attempt["lesson_url"]}\n' \
+                        f'Задание {negation}принято'
+                bot.send_message(text=message, chat_id=tg_settings.chat_id)
+        except Exception as error:
+            logger.error(error)
